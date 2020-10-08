4870fa26959ce73db9fbb0175b621703ce1be669
Restyled.io <commits@restyled.io>
2020-10-08 16:36:58 +0000

    Restyled by rubocop

--- Formula/dtrace.rb
+++ Formula/dtrace.rb
@@ -1,26 +1,28 @@
+# frozen_string_literal: true
+
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class DTract < Formula
-  desc "DTrace is a comprehensive dynamic tracing framework originally created by Sun Microsystems for troubleshooting kernel and application problems on production systems in real time. "
-  homepage "http://dtrace.org/"
-  url "ftp://crispeditor.co.uk/pub/release/website/dtrace/dtrace-20160613.tar.bz2"
-  version "20160613"
-  sha256 "1eeb8a10c3be6b57c2cc915f839fddb279380cd6f3462b312348c25331545a5b"
-  license "CDDL"
-  depends_on "perl"
-  depends_on "make"
+  desc 'DTrace is a comprehensive dynamic tracing framework originally created by Sun Microsystems for troubleshooting kernel and application problems on production systems in real time. '
+  homepage 'http://dtrace.org/'
+  url 'ftp://crispeditor.co.uk/pub/release/website/dtrace/dtrace-20160613.tar.bz2'
+  version '20160613'
+  sha256 '1eeb8a10c3be6b57c2cc915f839fddb279380cd6f3462b312348c25331545a5b'
+  license 'CDDL'
+  depends_on 'perl'
+  depends_on 'make'
 
   # depends_on "cmake" => :build
 
   def install
-    system "tools/get-deps.pl"
-    system "make all"
-    system "make install"
-    system "make load"
+    system 'tools/get-deps.pl'
+    system 'make all'
+    system 'make install'
+    system 'make load'
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
+    # system "./configure", "--disable-debug",
     #                      "--disable-dependency-tracking",
     #                      "--disable-silent-rules",
     #                      "--prefix=#{prefix}"
@@ -37,6 +39,6 @@ class DTract < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "true"
+    system 'true'
   end
 end
