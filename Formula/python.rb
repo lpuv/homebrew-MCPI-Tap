14f93d400e6659c54cc25c27aff9e56863d85864
Gamer4life <42851999+gamer4life1@users.noreply.github.com>
2020-10-08 11:23:42 -0700

    fix naming

--- Formula/python.rb
+++ Formula/python.rb
@@ -1,4 +1,4 @@
-class PythonAT37 < Formula
+class Python < Formula
   desc "Interpreted, interactive, object-oriented programming language"
   homepage "https://www.python.org/"
   url "https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tar.xz"
