1b236d6241ae29139ee66e63eecc816b8ba8d7d1
Gamer4life <42851999+gamer4life1@users.noreply.github.com>
2020-10-08 13:46:40 -0700

    fix folder

--- Formula/mcpiproxy.rb
+++ Formula/mcpiproxy.rb
@@ -15,6 +15,7 @@ class Mcpiproxy < Formula
   # depends_on "cmake" => :build
 
   def install
+     system "mkdir -p /home/linuxbrew/.linuxbrew/Cellar/custompython/3.7.9/lib/python3.7/dist-packages"
      system "cp ./proxy.py /home/linuxbrew/.linuxbrew/Cellar/custompython/3.7.9/lib/python3.7/dist-packages/mcpip.py"
      system "chmod a+x /home/linuxbrew/.linuxbrew/Cellar/custompython/3.7.9/lib/python3.7/dist-packages/mcpip.py"
      system "ln -s /home/linuxbrew/.linuxbrew/Cellar/custompython/3.7.9/lib/python3.7/dist-packages/mcpip.py #{prefix}/bin/mcpip"
